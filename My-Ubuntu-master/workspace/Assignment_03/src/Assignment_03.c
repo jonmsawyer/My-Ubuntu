12c070118256915d2e69d838fdf9876c13583bbf
Kairisawler <30772423+Kairisawler@users.noreply.github.com>
2018-02-06 11:00:06 -0400

    Add files via upload

---
+++ My-Ubuntu-master/workspace/Assignment_03/src/Assignment_03.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Assignment_03.c
+ Author      : Kai
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+void palin_func();
+
+int main(void) {
+	palin_func();
+
+	return EXIT_SUCCESS;
+}
+
