26abb5eba9035d686e027b330858fec2d319f85a
Jonathan Sawyer <jonmsawyer@gmail.com>
2018-03-29 16:18:43 -0800

    Updated code to reflect moving paddle

---
+++ PROG1400_Module_pong/make.bat
@@ -0,0 +1,1 @@
+javac -d bin src\Package_01\*.java 
--- PROG1400_Module_pong/src/Package_01/window.java
+++ PROG1400_Module_pong/src/Package_01/window.java
@@ -7,11 +7,12 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Event;
+import java.awt.event.KeyListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JTextField;
 
-public class window extends JFrame {
+public class window extends JFrame implements KeyListener {
 
 	/**
 	 * 
@@ -20,6 +21,11 @@ public class window extends JFrame {
 	private JTextField textFieldLeft;
 	private JTextField textFieldRight;
 	private JTextField textFieldHit;
+	private JTextField textFieldPaddle;
+	private JPanel panel_left;
+	private JPanel panel_right;
+	private JPanel panel_ball;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -30,52 +36,54 @@ public class window extends JFrame {
 					window frame = new window();
 					frame.setMinimumSize(new Dimension(440, 300));
 					frame.setVisible(true);
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
+	
+    public void keyReleased(KeyEvent event) {}    
+    public void keyTyped(KeyEvent event) {}
+    public void keyPressed(KeyEvent event)
+    {
+        int keyPress = event.getKeyCode();
+        int dy = 10;
+        System.out.println("Key was pressed! " + String.valueOf(keyPress));
+        
+        switch(keyPress)
+        {
+            case KeyEvent.VK_UP:
+                // minus delta y because Y coordinates are top to bottom
+                panel_left.setLocation(panel_left.getLocation().x, panel_left.getLocation().y - dy);
+                break;
+            case KeyEvent.VK_DOWN:
+                // plus delta y because Y coordinates are top to bottom
+                panel_left.setLocation(panel_left.getLocation().x, panel_left.getLocation().y + dy);
+                break;
+        }
+        
+        textFieldPaddle.setText("");
+    }
+    
 	/**
 	 * Create the frame.
 	 */
 	public window() {
 		getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-
-		panel.setBounds(0, 0, 440, 10);
-		getContentPane().add(panel);
-		
-		JPanel panel_left = new JPanel();
-		panel_left.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent event)
-			{
-				int keyPress = event.getKeyCode();
-				
-				switch(keyPress)
-				{
-					case KeyEvent.VK_UP:
-						panel_left.setLocation(panel_left.getLocation().x, panel_left.getLocation().y + 1);
-						break;
-						
-					case KeyEvent.VK_DOWN:
-						panel_left.setLocation(panel_left.getLocation().x, panel_left.getLocation().y - 1);
-						break;
-				}
-			}
-		});
+		panel_left = new JPanel();
 		panel_left.setBackground(Color.DARK_GRAY);
 		panel_left.setBounds(40, 85, 10, 95);
 		getContentPane().add(panel_left);
 		
-		JPanel panel_right = new JPanel();
+		panel_right = new JPanel();
 		panel_right.setBackground(Color.DARK_GRAY);
 		panel_right.setBounds(390, 85, 10, 95);
 		getContentPane().add(panel_right);
 		
-		JPanel panel_ball = new JPanel();
+		panel_ball = new JPanel();
 		panel_ball.setBackground(Color.RED);
 		panel_ball.setBounds(206, 114, 24, 24);
 		getContentPane().add(panel_ball);
@@ -97,6 +105,11 @@ public class window extends JFrame {
 		textFieldHit.setEditable(false);
 		getContentPane().add(textFieldHit);
 		textFieldHit.setColumns(10);
-
+		
+		textFieldPaddle = new JTextField();
+		textFieldPaddle.setBounds(0, 0, 10, 10);
+		textFieldPaddle.addKeyListener(this);
+		getContentPane().add(textFieldPaddle);
+		textFieldPaddle.setColumns(1);
 	}
 }
